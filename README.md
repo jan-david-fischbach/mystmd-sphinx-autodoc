37a54e41c24db821c465a56c0c501e82f043260d
Angus Hollands <goosey15@gmail.com>
2024-06-14 22:48:43 +0100

    wip

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sphinx demo with MyST-MD
+
+1. Install `python -m pip install -r requirements.txt`
+2. Install Node dependencies `npm install`
+2. Run MyST-MD `npx mystmd build`
--- autodoc.mjs
+++ autodoc.mjs
@@ -1,7 +1,7 @@
 import { spawn } from "node:child_process";
 import fs from "node:fs/promises";
 import { fromXml } from "xast-util-from-xml";
-
+import { selectAll } from "unist-util-select";
 
 function buildAutosummaryRST(node) {
   const bodyArgs = [];
@@ -22,6 +22,9 @@ ${body}
 `;
 }
 
+/**
+ * Interpret the parsed result of :foo: as an empty string, rather than "true"
+ */
 function coerceString(value) {
   switch (value) {
     case "true":
@@ -32,26 +35,64 @@ function coerceString(value) {
 }
 
 /**
- * Call out to an external process that conforms to a JSON-based
- * stdin-stdout transform specification.
- *
- * @param opts transform options, containing the binary path and arguments
+ * Prepare Sphinx and run a build
+ * @param opts transform options
  */
 function autodocTransformImpl(opts, utils) {
   return async (mdast) => {
     // TODO handle options
     const automoduleNodes = utils.selectAll("sphinx-automodule", mdast);
     const generatedDirectives = automoduleNodes.map(buildAutosummaryRST);
+    if (!automoduleNodes.length) {
+      return;
+    }
 
+    // Spit out index.rst
     await fs.writeFile("sphinx/index.rst", generatedDirectives.join("\n"));
 
-    const subprocess = spawn("sphinx-build", ["-b", "xml", "sphinx", "sphinx/xml"]);	
-await new Promise((resolve) => {
-      subprocess.on('close', resolve);
+    // Run Sphinx build
+    const subprocess = spawn("sphinx-build", [
+      "-b",
+      "xml",
+      "sphinx",
+      "sphinx/xml",
+    ]);
+    await new Promise((resolve) => {
+      subprocess.on("close", resolve);
     });
-    const tree = fromXml(await fs.readFile("sphinx/xml/index.xml")) ;
-    console.log(JSON.stringify(tree, null, 2))
+
+    // Parse the resulting XML
+    const tree = fromXml(await fs.readFile("sphinx/xml/index.xml"));
+
+    // The actual data follows the target. We want something like target + text + desc as a selector, but I don't know how robust that is.
+    const descNodes = selectAll("element[name=desc]", tree);
+
+    // Group `desc` nodes by module
+    const moduleToDesc = new Map();
+    descNodes.forEach((node) => {
+      // Parse `module-XXX` ID as `XXX`
+      const refID = node.attributes?.ids ?? ""; // TODO: is this plural, i.e separated somehow? Assume not.
+      const [_, module] = refID.match(/module-(.*)/);
+
+      // Append desc node to those grouped by module
+      if (moduleToDesc.has(module)) {
+        moduleToDesc.get(module).push(node);
+      } else {
+        moduleToDesc.set(module, [node]);
+      }
+    });
+
+    // Now process each node
+    automoduleNodes.forEach((node) => {
+      const moduleDescNodes = moduleToDesc.get(node.module);
+      processModule(node, moduleDescNodes);
+    });
   };
+}
+
+function processModule(node, descNodes) {
+  console.log(node.module, JSON.stringify(descNodes, null, 2));
+  node.children = descNodes;
 }
 
 const automoduleDirective = {
